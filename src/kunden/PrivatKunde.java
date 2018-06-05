33f0267d9776b33201908395936fe3c0a11d9d0a
Jannis Kiesel <jannis.kiesel@outlook.de>
2018-06-05 11:15:07 +0200

    beienflusste Privatkunden

--- src/kunden/PrivatKunde.java
+++ src/kunden/PrivatKunde.java
@@ -1,6 +1,12 @@
 package kunden;
 
+import java.util.Date;
+
 public class PrivatKunde extends Kunde{
+	
+	private String vorname;
+	private String nachname;
+	private Date geburtstag;
 
 	public PrivatKunde() {
 		// TODO Auto-generated constructor stub
