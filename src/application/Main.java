3df772e05e2ad31aeaeaea693be2124db9cadafe
Jannis Kiesel <jannis.kiesel@outlook.de>
2018-06-11 10:05:42 +0200

    Funktionale Anforderungen erweitert

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,88 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import kunden.BusinessKunde;
+import kunden.Kunde;
+import kunden.PrivatKunde;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 
 public class Main extends Application {
+	private TableView tabelle = createTable();
+	private ObservableList<Kunde> data = createObservableData();
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			tabelle.setItems(data);
+			root.setCenter(tabelle);
+			addAdd(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+	private ObservableList<Kunde> createObservableData() {
+		ObservableList<Kunde> temp = FXCollections.observableArrayList();
+		return temp;
+	}
+	private void addAdd(BorderPane root) {
+		final TextField addFirstName = new TextField();
+		addFirstName.setPromptText("First Name");
+		final TextField addLastName = new TextField();
+		addLastName.setPromptText("Last Name");
+		final TextField addEmail = new TextField();
+		addEmail.setPromptText("Email");
+		 
+		final Button addButton = new Button("Add");
+		addButton.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		        data.add(new PrivatKunde(
+		            Long.parseLong(addFirstName.getText()),
+		            addEmail.getText()
+		        ));
+		        addFirstName.clear();
+		        addLastName.clear();
+		        addEmail.clear();
+		    }
+		});
+		root.setBottom(new HBox(addFirstName,addEmail,addButton));
+	}
+	
+	private TableView createTable() {
+		TableView<Kunde> temp = new TableView<Kunde>();
+		temp.setEditable(true);
+		TableColumn<Kunde, String> firstNameCol = new TableColumn<Kunde, String>("First Name");
+        TableColumn<Kunde, String> lastNameCol = new TableColumn<Kunde, String>("Last Name");
+        TableColumn<Kunde, String> emailCol = new TableColumn<Kunde, String>("Email");
+        temp.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+        
+        firstNameCol.setCellValueFactory(
+        	    new PropertyValueFactory<Kunde,String>("kundenid")
+        	);
+        	emailCol.setCellValueFactory(
+        	    new PropertyValueFactory<Kunde,String>("email")
+        	);
+		return temp;
 	}
 	
 	public static void main(String[] args) {
--- src/kunden/Kunde.java
+++ src/kunden/Kunde.java
@@ -6,13 +6,43 @@ import javafx.collections.ObservableList;
 
 public abstract class Kunde {
 	
-	private long kundennummer;
-	private String kontaktEmail;
-	ObservableList<Auftrag> auftraege;
+	private long kundenid;
+	private String email;
+	//ObservableList<Auftrag> auftraege;
 	
-	
-	public Kunde() {
-		
+	public long getKundenid() {
+		return kundenid;
 	}
 
+
+	public void setKundenid(long kundenid) {
+		this.kundenid = kundenid;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+//	public ObservableList<Auftrag> getAuftraege() {
+//		return auftraege;
+//	}
+//
+//
+//	public void setAuftraege(ObservableList<Auftrag> auftraege) {
+//		this.auftraege = auftraege;
+//	}
+
+
+	public Kunde(long kundenid, String email) {
+		super();
+		this.kundenid = kundenid;
+		this.email = email;
+	}
 }
